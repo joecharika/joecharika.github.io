cc8037ad1b608909111886047a36743c99fcd68f
Joseph Chareka <42941541+joecharika@users.noreply.github.com>
2022-01-03 19:29:59 +0200

    Data age demo

---
+++ demo/data-age/assets/hire-us.ce7a7837.js
@@ -0,0 +1,1 @@
+var s="/assets/hire-us.2af38d99.jpg";export{s as _};
---
+++ demo/data-age/assets/our-work.a48ca902.js
@@ -0,0 +1,1 @@
+import{_ as r}from"./plugin-vue_export-helper.21dcd24c.js";const e={name:"OurWork"};function o(t,c,n,p,s,_){return"Our Work"}var u=r(e,[["render",o]]);export{u as default};
